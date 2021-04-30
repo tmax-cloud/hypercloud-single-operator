45a81dba7bfa10a12e3936287fa26c16b78abd35
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-04-30 15:42:19 +0900

    [mod] NS 생성 오류시 status가 error로 바뀌지 않는 버그 수정

--- api/v1alpha1/namespaceclaim_webhook.go
+++ api/v1alpha1/namespaceclaim_webhook.go
@@ -58,7 +58,7 @@ func (r *NamespaceClaim) ValidateUpdate(old runtime.Object) error {
 	old_status := old.(*NamespaceClaim).DeepCopy().Status.Status
 	now_status := r.Status.Status
 
-	if (old_status == NamespaceClaimStatusTypeSuccess && (now_status != NamespaceClaimStatusTypeSuccess && now_status != NamespaceClaimStatueTypeDeleted)) ||
+	if (old_status == NamespaceClaimStatusTypeSuccess && (now_status != NamespaceClaimStatusTypeSuccess && now_status != NamespaceClaimStatueTypeDeleted && now_status != NamespaceClaimStatueTypeError)) ||
 		(old_status == NamespaceClaimStatueTypeDeleted && now_status != NamespaceClaimStatueTypeDeleted) {
 		return errors.NewForbidden(
 			schema.GroupResource{Group: "claim.tmax.io", Resource: r.Name},
