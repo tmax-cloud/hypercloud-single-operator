749ab031851f7c6b48d6d00c4dcdd0e041979e32
Seungwon Lee <seungwon_lee@tmax.co.kr>
2021-09-09 15:12:06 +0900

    [etc] CLI 테스트 방법 추가

---
+++ test/CLI-patch-test.sh
@@ -0,0 +1,21 @@
+# 1. 클러스터 이름을 참조하는 API 서버를 가리킨다.
+CLUSTER_NAME = kubectl config view -o jsonpath="{.clusters[0].name}"
+APISERVER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$CLUSTER_NAME\")].cluster.server}")
+
+
+# 2. 토큰 값을 얻는다
+TOKEN=$(kubectl get secrets -n hypercloud5-system -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='hypercloud5-admin')].data.token}"|base64 --decode)
+
+
+# 3. TOEKN으로 API 콜
+if [ -z "$3" ]
+then
+    # 3-1. cluster-scope의 리소스인 경우
+    # $1={CRD 이름} $2={리소스 이름}
+    curl -X PATCH "$APISERVER/apis/claim.tmax.io/v1alpha1/$1/$2/status" -H "Content-Type: application/json-patch+json" -H "Authorization: Bearer $TOKEN" -k -d '[{"op": "replace", "path": "/status/status", "value": "Approved"}]'
+else
+    # 3-2. namespace-scope의 리소스인 경우
+    # $1={CRD 이름} $2={리소스 이름} $3={네임스페이스}
+    curl -X PATCH "$APISERVER/apis/claim.tmax.io/v1alpha1/namespaces/$3/$1/$2/status" -H "Content-Type: application/json-patch+json" -H "Authorization: Bearer $TOKEN" -k -d '[{"op": "replace", "path": "/status/status", "value": "Approved"}]'
+fi
+
---
+++ test/readme.md
@@ -0,0 +1,24 @@
+## CLI를 통한 claim류 patch 테스트
+1. sample-nsc 생성, hypercloud5-admin sa에 권한 부여
+    ```
+    $ kubectl apply -f CLI-patch-test.sh
+    ```
+
+2. 실행  
+* 2-1. ClusterScope인 경우
+    ```
+    $ ./CLI-patch-test.sh {CRD 이름} {리소스 이름}
+    ```
+    예시
+    ```
+    $ ./CLI-patch-test.sh namespaceclaims swlee-test-nsc
+    ```
+
+* 2-2. NamespaceScope인 경우
+    ```
+    $ ./CLI-patch-test.sh {CRD 이름} {리소스 이름} {네임스페이스}
+    ```
+    예시
+    ```
+    $ ./CLI-patch-test.sh resourcequotaclaims swlee-test-nsc swlee-test
+    ```
