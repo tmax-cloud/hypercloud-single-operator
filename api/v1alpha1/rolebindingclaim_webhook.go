18df46915b3f7a5cdc3263a65db80057e0cd1cba
Seungwon Lee <seungwon_lee@tmax.co.kr>
2022-08-27 17:48:20 +0900

    [mod] make 명령어 시, 웹훅 설정 추가되도록 kubebuilder 주석 수정

--- api/v1alpha1/rolebindingclaim_webhook.go
+++ api/v1alpha1/rolebindingclaim_webhook.go
@@ -39,8 +39,7 @@ func (r *RoleBindingClaim) SetupWebhookWithManager(mgr ctrl.Manager) error {
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-//+kubebuilder:webhook:verbs=create;update,path=/validate-claim-tmax-io-v1alpha1-rolebindingclaim,mutating=false,failurePolicy=fail,groups=claim.tmax.io,resources=rolebindingclaims/status,versions=v1alpha1,name=vrolebindingclaim.kb.io,admissionReviewVersions=v1beta1;v1,sideEffects=NoneOnDryRun
-//+kubebuilder:webhook:verbs=create;update,path=/validate-claim-tmax-io-v1alpha1-namespaceclaim,mutating=false,failurePolicy=fail,groups=claim.tmax.io,resources=namespaceclaims;namespaceclaims/status,versions=v1alpha1,name=vnamespaceclaim.kb.io,admissionReviewVersions=v1beta1;v1,sideEffects=NoneOnDryRun
+//+kubebuilder:webhook:verbs=create;update;delete,path=/validate-claim-tmax-io-v1alpha1-rolebindingclaim,mutating=false,failurePolicy=fail,groups=claim.tmax.io,resources=rolebindingclaims;rolebindingclaims/status,versions=v1alpha1,name=vrolebindingclaim.kb.io,admissionReviewVersions=v1beta1;v1,sideEffects=NoneOnDryRun
 
 var _ webhook.Validator = &RoleBindingClaim{}
 
